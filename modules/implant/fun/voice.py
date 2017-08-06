2cac5bc37a9eb8b63229cfe37c51b2d8e7476151
root <root@fbctf.home>
2017-08-05 21:18:32 -0300

    Fixing Voice

--- modules/implant/fun/voice.py
+++ modules/implant/fun/voice.py
@@ -16,9 +16,17 @@ class VoiceImplant(core.implant.Implant):
     AUTHORS = ["RiskSense, Inc."]
 
     def load(self):
-        self.options.register("MESSAGE", "I can't do that Dave", "message to speak")
+        self.options.register("messsage", "I can't ddo that Dave", "message to speak")
 
     def run(self):
+
+        try:
+            with open("data/implant/fun/voice.js", "w") as file: 
+                voice = "try {var voiceObj = new ActiveXObject('sapi.spvoice'); for (var i = 0; i < 50; ++i) {Koadic.WS.SendKeys(String.fromCharCode(0xAF)); } voiceObj.Speak("+self.options.get('message')+");Koadic.work.report(''); } catch (e) {Koadic.work.error(e) } Koadic.exit();"
+                file.write(voice)
+                file.close()
+        except IOError as e:
+            raise e
 
         payloads = {}
         #payloads["vbs"] = self.load_script("data/implant/fun/voice.vbs", self.options)
